5015b9795a6b5886703115f240da624a2d5e4f4e
gustavo <guga112006@gmail.com>
2024-08-03 15:42:16 -0300

    move att

--- mob/player_script.gd
+++ mob/player_script.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
-var speed = 400
+var speed = 100
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_up"):
-		velocity.y =  - speed
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = direction * speed
+	print(direction)
+	
+	
+	move_and_slide()
