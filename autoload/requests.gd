845ab14344106a1e24192943168e71fab8c25c06
FelipeBdC <batatanotube@gmail.com>
2024-08-04 16:36:22 -0300

    improved scene manager script

--- autoload/requests.gd
+++ autoload/requests.gd
@@ -2,3 +2,4 @@ extends Node
 
 
 signal change_scene(scene: PackedScene)
+signal change_ui(scene: PackedScene)
--- scene_manager.gd
+++ scene_manager.gd
@@ -1,26 +1,38 @@
-extends Node2D
+extends Node
 
 
+@export var change_scene_request: String = "change_scene"
 @export var default_scene: PackedScene
 
 
 func _ready() -> void:
-	Requests.change_scene.connect(change_scene)
+	Requests.get(change_scene_request).connect(change_scene)
 	add_scene(default_scene)
 
 
 func change_scene(scene_file: PackedScene) -> void:
-	await remove_scene().tree_exited
+	var old_scene = remove_scene()
+	if is_instance_valid(old_scene):
+		await old_scene.tree_exited
 	add_scene(scene_file)
 
 
 func remove_scene() -> Node2D:
+	if get_child_count() == 0:
+		return null
 	var current_scene: Node2D = get_child(0)
+	if not is_instance_valid(current_scene):
+		return null
+	
 	current_scene.queue_free()
 	return current_scene
 
 
 func add_scene(scene_file: PackedScene) -> Node2D:
+	if not scene_file:
+		return null
+	
 	var scene = scene_file.instantiate()
 	self.add_child(scene)
 	return scene
+
