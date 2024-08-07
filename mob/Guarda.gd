176dbab5f8219aac48be4d9778b2cbe84d5d6aae
gustavo <guga112006@gmail.com>
2024-08-06 23:20:03 -0300

    guarda sistem update

--- mob/Guarda.gd
+++ mob/Guarda.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
 
 
-const SPEED =200
-var waypoint: Vector2
-var ponto_ir: Vector2 = $Marker2D.global_position
-var current_position: vector = 
+const speed = 50
+
+@onready var waypoint: Vector2 = $Marker2D.global_position
+@onready var first_position: Vector2 = global_position
+@onready var direction = waypoint - first_position
+
+func _ready():
+	pass
+	
 func _physics_process(delta):
-	while CharacterBody2D.glbal_position != pontoir:
-		velocity.x -= speed
-
-
+	var current_position: Vector2 = global_position
+	
+	if  global_position.distance_to(waypoint) < 5:
+		direction = Vector2.ZERO
+		
+	print(current_position)
+	velocity = direction * speed * delta
+	
 	move_and_slide()
